3e978df4bc491e8dff9e1b7c2e7ae10dcedd4524
paul-zykovsky <zykov.pv@yandex.ru>
2020-09-24 12:48:24 +0300

    Задания к первому уроку, C#

---
+++ Hometasks/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/* Студент: Зыков Павел. ДЗ №1: программа "Анкета". 
+ */
+namespace Task1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string first_name, last_name, age, height, weight;
+
+            Console.Write("Ваше имя: ");
+            first_name = Console.ReadLine();
+            Console.Write("Ваша фамилия: ");
+            last_name = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            age = Console.ReadLine();
+            Console.Write("Ваш рост: ");
+            height = Console.ReadLine();
+            Console.Write("Ваш вес: ");
+            weight = Console.ReadLine();
+
+            Console.WriteLine(first_name + " " + last_name + 
+                              ", возраст: " + age + ", рост: "
+                              + height + ", вес: " + weight);
+            Console.WriteLine("{0} {1}, возраст: {2}, рост: {3}, вес: {4}",
+                              first_name, last_name, age, height, weight);
+            Console.WriteLine($"{first_name} {last_name}, возраст: {age}, рост: {height}, вес: {weight}");
+
+            return 0;
+        }
+    }
+}
---
+++ Hometasks/Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/* Студент: Зыков Павел. ДЗ №5: программа "Форматированный вывод". 
+ * Подвариант б).
+ */
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(35, 14);
+            Console.WriteLine("Иван Семеныч Параллелограмм, село Горелово.");
+        }
+    }
+}
