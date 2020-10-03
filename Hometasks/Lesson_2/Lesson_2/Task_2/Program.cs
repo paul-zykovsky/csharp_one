55a59858d38f786a1f75db2df289ea55c3819efd
paul-zykovsky <zykov.pv@yandex.ru>
2020-10-03 11:17:03 +0300

    Задания к уроку №2

---
+++ Hometasks/Lesson_2/Lesson_2/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/* Студент: Зыков Павел. Урок 2, задание 2, метод возвращает 
+ * количество цифр числа.
+ */
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Кол-во цифр числа {a} : {CntDigits(a)}");
+        }
+
+        static int CntDigits(int a)
+        {
+            int count = (a == 0)? 1 : 0;
+            for (; a != 0; count++, a /= 10)
+                ;
+            return count;
+        }
+    }
+}
---
+++ Hometasks/Lesson_2/Lesson_2/Task_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/* Студент: Зыков Павел. Урок 2, задание 5, проверка ИМТ.
+ */
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight, height, weight_norm;
+            double idx, sqr_height;
+
+            weight = int.Parse(Console.ReadLine());
+            height = int.Parse(Console.ReadLine());
+            sqr_height = Math.Pow(height / 100.0, 2);
+            idx = weight / sqr_height;
+            Console.WriteLine($"Индекс: {idx}");
+
+            if (idx < 18.5)
+            {
+                weight_norm = (int)(18.5 * sqr_height);
+                Console.WriteLine($"Наберите {weight_norm - weight} кг");
+            }
+            else if (idx <= 25)
+            {
+                Console.WriteLine("Вес в норме");
+            }
+            else {
+                weight_norm = (int)(25 * sqr_height);
+                Console.WriteLine($"Сбросьте {weight - weight_norm} кг");
+            }
+
+        }
+    }
+}
