3de0a480d80d4b7e03f44c4ee7d46e3f77992eb4
paul-zykovsky <zykov.pv@yandex.ru>
2020-10-04 03:26:42 +0300

    Задания к уроку №3.

---
+++ Hometasks/Lesson_3/Task_1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+/* Студент: Зыков Павел, занятие №3, задача 1, Модификация структуры/класса Complex.
+ */
+namespace Task_1
+{
+    struct Complex_struct
+    {
+        public double im, re;
+        public Complex_struct Plus(Complex_struct x)
+        {
+            Complex_struct y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+
+        public Complex_struct Minus(Complex_struct x)
+        {
+            Complex_struct y;
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+
+        public Complex_struct Multiple(Complex_struct x)
+        {
+            Complex_struct y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+
+    class Complex_class
+    {
+        private double im, re;
+
+        public Complex_class() { im = re = 0; }
+
+        public Complex_class(double re, double im)
+        {     
+            this.im = im;
+            this.re = re;
+        }
+        public Complex_class Plus(Complex_class x2)
+        {
+            Complex_class x3 = new Complex_class();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        public Complex_class Minus(Complex_class x2)
+        {
+            Complex_class x3 = new Complex_class();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+
+        public Complex_class Multiple(Complex_class x2)
+        {
+            Complex_class x3 = new Complex_class();
+            x3.re = re * x2.re - im * x2.im;
+            x3.im = im * x2.re + re * x2.im;
+            return x3;
+        }
+
+        // Свойства - это механизм доступа к данным класса.
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                // Для примера ограничимся только положительными числами.
+                if (value >= 0) im = value;
+            }
+        }
+        
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Complex как структура ===");
+            Complex_struct a, b;
+            a.re = 7; a.im = 3;
+            b.re = 2; b.im = 1;
+            Console.WriteLine($"Сложение: {(a.Plus(b)).ToString()}");
+            Console.WriteLine($"Вычитание: {(a.Minus(b)).ToString()}");
+            Console.WriteLine($"Умножение: {(a.Multiple(b)).ToString()}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("=== Complex как класс ===");
+            Complex_class c = new Complex_class(7, 3), d = new Complex_class(2, 1);
+            Console.WriteLine($"Сложение: {(c.Plus(d)).ToString()}");
+            Console.WriteLine($"Вычитание: {(c.Minus(d)).ToString()}");
+            Console.WriteLine($"Умножение: {(c.Multiple(d)).ToString()}");
+        }
+    }
+}
---
+++ Hometasks/Lesson_3/Task_3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+/* Студент: Зыков Павел, занятие №3, задача 3,  класс "Дробь".
+ */
+namespace Task_3
+{
+    class Fraction
+    {
+        private int    a;    // numerator;
+        private int    b;    // denomerator;
+        private double value;// double representation;
+
+        public int A
+        {
+            get { return a; }
+            set 
+            { 
+                a = value;
+                this.value = (double)a / b;
+            }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                try
+                {
+                    b = value;
+                    if (b == 0)
+                        throw new ArgumentException("Знаменатель не может быть равен 0", "b");
+                    this.value = (double)a / b;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("{0}: {1}", e.GetType().Name, e.Message);
+                }
+            }
+        }
+
+        public double Value
+        {
+            get { return value; }
+            private set { this.value = value; }
+        }
+
+        public Fraction() { a = 0; b = 1; value = 0.0; }
+
+        public Fraction(int a, int b) { A = a; B = b; }
+
+        public Fraction Plus(Fraction add)
+        {
+            Fraction result = new Fraction();
+            result.b = b * add.b;
+            result.a = add.a * b + a * add.b;
+            return result;
+        }
+
+        public Fraction Minus(Fraction dec)
+        {
+            Fraction result = new Fraction();
+            result.b = b * dec.b;
+            result.a = a * dec.b - b * dec.a;
+            return result;
+        }
+
+        public Fraction Multiple(Fraction with)
+        {
+            Fraction result = new Fraction();
+            result.a = a * with.a;
+            result.b = b * with.b;
+            return result;
+        }
+
+        public Fraction Divide(Fraction onto)
+        {
+            Fraction result = new Fraction();
+            result.a = a * onto.b;
+            result.b = b * onto.a;
+            return result;
+        }
+
+        public string toString() { return a + "/" + b; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(1, 3), b = new Fraction(1, 3);
+            Console.WriteLine("Fraction A = 1/3, B = 1/3 ");
+            Console.WriteLine($"A + B = {a.Plus(b).toString()}");
+            Console.WriteLine($"A - B = {a.Minus(b).toString()}");
+            Console.WriteLine($"A * B = {a.Multiple(b).toString()}");
+            Console.WriteLine($"A : B = {a.Divide(b).toString()}");
+
+            Console.WriteLine("C = 1/0");
+            Fraction c = new Fraction(1, 0);
+        }
+    }
+}
